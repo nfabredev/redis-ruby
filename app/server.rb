5ed0641a395538042aaf105eb57366a28229e505
Nicolas Fabre <nfabredev@gmail.com>
2023-12-13 08:44:16 +0200

    Stage #4: Handle concurrent clients

--- app/server.rb
+++ app/server.rb
@@ -4,22 +4,32 @@ class RedisServer
   MAX_REQUEST_LENGTH = 1024
   def initialize(port)
     @port = port
+    @server = TCPServer.new(port)
+    @clients = []    
   end
 
-  def start
-
-    server = TCPServer.new(@port)
-    s = server.accept
-
+  def listen
     loop do
-      s.recv(MAX_REQUEST_LENGTH)
-      s.puts "+PONG\r\n"
-      rescue Errno::ECONNRESET
-        puts "The connection is terminated by the client."
-      break
+      fds_to_watch = [@server, *@clients]
+      ready_to_read, _, _ = IO.select(fds_to_watch)
+      ready_to_read.each do |ready|
+        if ready === @server
+          @clients << @server.accept
+        else
+          handle_client(ready)
+        end
+      end
     end
-    s.close
+  end
+
+  def handle_client(client)
+    client.readpartial(MAX_REQUEST_LENGTH)
+    client.write("+PONG\r\n")
+    rescue EOFError
+      client.close
+      @clients.delete(client)
+      puts "Client disconnected."
   end
 end
 
-RedisServer.new(6379).start
+RedisServer.new(6379).listen
