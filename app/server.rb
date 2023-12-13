6d7d39a69574d338fab8f02d1b533d2142524bbf
Nicolas Fabre <nfabredev@gmail.com>
2023-12-13 08:03:01 +0200

    Stage #3 Respond to multiple PINGs

--- app/server.rb
+++ app/server.rb
@@ -1,6 +1,7 @@
 require "socket"
 
-class YourRedisServer
+class RedisServer
+  MAX_REQUEST_LENGTH = 1024
   def initialize(port)
     @port = port
   end
@@ -9,9 +10,16 @@ class YourRedisServer
 
     server = TCPServer.new(@port)
     s = server.accept
-    s.puts "+PONG\r\n"
+
+    loop do
+      s.recv(MAX_REQUEST_LENGTH)
+      s.puts "+PONG\r\n"
+      rescue Errno::ECONNRESET
+        puts "The connection is terminated by the client."
+      break
+    end
     s.close
   end
 end
 
-YourRedisServer.new(6379).start
+RedisServer.new(6379).start
