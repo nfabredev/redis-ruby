04c999774e70ad3424edec5c8f661a6ceaceed58
Nicolas Fabre <nfabredev@gmail.com>
2023-12-13 12:14:03 +0200

    Stage #5: Implement the ECHO command

--- app/server.rb
+++ app/server.rb
@@ -1,5 +1,51 @@
 require "socket"
 
+class Parser
+  SKIPPED_STRING_DESCRIPTOR = ['$', '*']
+  
+  def initialize(message)
+    @message = message
+    @response = ''
+    @last_processed_command = ''
+  end
+
+  def parse
+    puts "Message to parse - #{@message}"
+    @message.split("\r\n").each_with_index do |command|
+      process_command(command)
+    end
+    puts "Response - #{@response}"
+    @response
+  end
+  
+  def process_command(command)
+    puts "Command - #{command}"
+    command_type = command[0]
+    puts "command type = #{command_type}"
+    return if SKIPPED_STRING_DESCRIPTOR.include?(command_type)
+
+    case command.upcase
+      when 'PING'
+        process_ping(command.upcase)
+    end
+
+    case @last_processed_command
+    when 'echo'
+      process_simple_string(command)
+    end
+    
+    @last_processed_command = command
+  end
+  
+  def process_ping(_command)
+    process_simple_string('PONG')
+  end
+
+  def process_simple_string(display_string)
+    @response += "+#{display_string}\r\n"
+  end
+end
+
 class RedisServer
   MAX_REQUEST_LENGTH = 1024
   def initialize(port)
@@ -23,8 +69,9 @@ class RedisServer
   end
 
   def handle_client(client)
-    client.readpartial(MAX_REQUEST_LENGTH)
-    client.write("+PONG\r\n")
+    request = client.readpartial(MAX_REQUEST_LENGTH)
+    response = Parser.new(request).parse
+    client.write(response)
     rescue EOFError
       client.close
       @clients.delete(client)
