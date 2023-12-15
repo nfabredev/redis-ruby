817c27356fb89a3d7b57d2afe31f0fd3ebcbea34
Nicolas Fabre <nfabredev@gmail.com>
2023-12-15 09:55:04 +0200

    chore: linting:
    - running rubocop

--- app/server.rb
+++ app/server.rb
@@ -1,8 +1,10 @@
-require "socket"
+# frozen_string_literal: true
+
+require 'socket'
 
 class Parser
-  SKIPPED_STRING_DESCRIPTOR = ['$', '*']
-  
+  SKIPPED_STRING_DESCRIPTOR = ['$', '*'].freeze
+
   def initialize(message)
     @message = message
     @response = ''
@@ -11,32 +13,40 @@ class Parser
 
   def parse
     puts "Message to parse - #{@message}"
-    @message.split("\r\n").each_with_index do |command|
+    @message.split("\r\n").reject { |element| (element =~ /[*$]\d+/) }.each_with_index do |command, _index|
       process_command(command)
     end
     puts "Response - #{@response}"
     @response
   end
-  
+
   def process_command(command)
     puts "Command - #{command}"
     command_type = command[0]
     puts "command type = #{command_type}"
-    return if SKIPPED_STRING_DESCRIPTOR.include?(command_type)
 
     case command.upcase
-      when 'PING'
-        process_ping(command.upcase)
+    when 'PING'
+      process_ping(command.upcase)
     end
 
     case @last_processed_command
-    when 'echo'
+    when 'ECHO'
       process_simple_string(command)
+    when 'SET'
+      process_set(command)
     end
-    
-    @last_processed_command = command
+
+    @last_processed_command = command.upcase
   end
-  
+
+  def process_set(_command)
+    key = all_commands[index]
+    value = all_commands[index + 2]
+    Storage.instance.set_value(key, value)
+    process_simple_string('OK')
+  end
+
   def process_ping(_command)
     process_simple_string('PONG')
   end
@@ -51,13 +61,13 @@ class RedisServer
   def initialize(port)
     @port = port
     @server = TCPServer.new(port)
-    @clients = []    
+    @clients = []
   end
 
   def listen
     loop do
       fds_to_watch = [@server, *@clients]
-      ready_to_read, _, _ = IO.select(fds_to_watch)
+      ready_to_read, = IO.select(fds_to_watch)
       ready_to_read.each do |ready|
         if ready === @server
           @clients << @server.accept
@@ -72,10 +82,10 @@ class RedisServer
     request = client.readpartial(MAX_REQUEST_LENGTH)
     response = Parser.new(request).parse
     client.write(response)
-    rescue EOFError
-      client.close
-      @clients.delete(client)
-      puts "Client disconnected."
+  rescue EOFError
+    client.close
+    @clients.delete(client)
+    puts 'Client disconnected.'
   end
 end
 
